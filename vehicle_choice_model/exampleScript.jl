8243ba8265b77a70aa9a67808aa24a8d4556ad94
lquante <lennart.quante@me.com>
2021-06-14 10:00:25 +0200

    including random number seed into example script

--- vehicle_choice_model/exampleScript.jl
+++ vehicle_choice_model/exampleScript.jl
@@ -5,17 +5,15 @@ include("populationCreation.jl")
 include("visualization.jl")
 
 
-diverseGaia = model_car_owners(mixed_population)
-
-interactive_simulation(diverseGaia,agent_step!,model_step!)
-
-Agents.step!(gaiaOeconomicus,agent_step!,model_step!,1) # stepping to test, wheither model setup is working
-
-
 # test with more agents
 
 space = Agents.GridSpace((30, 30); periodic = true, metric = :euclidean)
 
 mixedHugeGaia = model_car_owners(electric_minority;space=space)
+
+# set random number seed
+
+seed!(mixedHugeGaia,19956060601032517)
+
 
 interactive_simulation(mixedHugeGaia,agent_step!,model_step!)
