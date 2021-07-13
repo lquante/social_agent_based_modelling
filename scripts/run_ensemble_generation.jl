175ee24ed1d82feacf1886b36ff675fb10708db4
lquante <lennart.quante@me.com>
2021-07-13 14:38:36 +0200

    fix global run counter

--- scripts/run_ensemble_generation.jl
+++ scripts/run_ensemble_generation.jl
@@ -16,10 +16,10 @@ seeds = rand(1234:9999,100)
 p_normal_dist = truncated(Normal(0.5, 0.05), 0.3, 0.6)
 p_combustion_range = rand(p_normal_dist, 100)
 plot_combustion_share_histogram(p_combustion_range, plotsdir("histogram_p_combustion.png"))
-run = 1
+run = 0
 for p in p_combustion_range
+    global run +=1
     runpath = datadir(savename("model_generation_",@ntuple p run))
-    run +=1
     mkpath(runpath)
     schedule_script(script=scriptsdir("run_single_model_generation.jl")*"--$p",workdir=runpath)
 end
