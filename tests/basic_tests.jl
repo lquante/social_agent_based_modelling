e7f6e107819d6c104852876f2993e92a608338a5
lquante <lennart.quante@me.com>
2021-07-14 18:46:00 +0200

    small update of tests

--- tests/basic_tests.jl
+++ tests/basic_tests.jl
@@ -17,28 +17,29 @@ tagent=test.agents[23]
 @testset "agentFunctions.jl" begin
     @test get_car_price(0,test) == test.priceCombustionCar
     @test get_car_price(1,test) == test.priceElectricCar
+    test_iterations  = 100
     for state in [0,1]
         set_state!(state,tagent)
         @test tagent.state == state
     end
-    for carValue in rand(0:0.5:10000,10)
+    for carValue in rand(0:0.5:10000,test_iterations)
         set_carValue!(carValue,tagent)
         @test tagent.carValue == carValue
         set_purchaseValue!(carValue,tagent)
         @test tagent.purchaseValue == carValue
     end
-    for carAge in rand(0:100,10)
+    for carAge in rand(0:100,test_iterations)
         set_carAge!(carAge,tagent)
         @test tagent.carAge == carAge
     end
-    for budgetChange in rand(0:10000,10)
+    for budgetChange in rand(0:10000,test_iterations)
         previousBudget = tagent.budget
         update_budget!(budgetChange,tagent)
         @test tagent.budget == previousBudget-budgetChange
     end
-    for new_budget in rand(0:0.25:10000,10)
+    for new_budget in rand(0:0.25:10000,test_iterations)
         set_budget!(new_budget,tagent)
         @test tagent.budget == new_budget
     end
-    
+
 end
