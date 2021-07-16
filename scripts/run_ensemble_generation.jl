c97cf6a8e49163d10aacbb55ced2dbeb9c1d6ada
a.stechemesser <stechemesser@pik-potsdam.de>
2021-07-16 10:05:33 +0200

    ensemble plotting

--- scripts/run_ensemble_generation.jl
+++ scripts/run_ensemble_generation.jl
@@ -20,3 +20,12 @@ p_combustion_range = rand(p_normal_dist, 100)
 plot_combustion_share_histogram(p_combustion_range, plotsdir("histogram_p_combustion.png"))
 
 generate_ensemble(p_combustion_range,datadir();step_length=50,gridsize = 30, models_per_p = 100,seeds = rand(1234:9999,100),store_model = false, model_directory = "")
+
+##load results and make some plots
+ensemble_data = load_results_data("C:\\Users\\stecheme\\Documents\\Social_Modelling\\ensemble_granular_oscillation_convergence_normal_dist_3\\")
+
+plot_scatter(ensemble_data, "C:\\Users\\stecheme\\Documents\\Social_Modelling\\ensemble_granular_oscillation_convergence_normal_dist_3\\test_state.png")
+plot_scatter(ensemble_data, "C:\\Users\\stecheme\\Documents\\Social_Modelling\\ensemble_granular_oscillation_convergence_normal_dist_3\\test_affinity.png"; variable = ensemble_data.Final_Affinity_Average,y_lab = "Final Affinity Average")
+
+plot_histogram(ensemble_data, "C:\\Users\\stecheme\\Documents\\Social_Modelling\\ensemble_granular_oscillation_convergence_normal_dist_3\\test_hist_state.png")
+plot_histogram(ensemble_data, "C:\\Users\\stecheme\\Documents\\Social_Modelling\\ensemble_granular_oscillation_convergence_normal_dist_3\\test_hist_affinity.png"; variable = ensemble_data.Final_Affinity_Average,y_lab = "Final Affinity Average")
