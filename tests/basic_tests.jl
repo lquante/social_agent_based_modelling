2b8cd2b0e3e598d84f031c6260b69bdae21ee20f
lquante <lennart.quante@me.com>
2021-09-08 09:25:48 +0200

    generalizing basic test file

--- tests/basic_tests.jl
+++ tests/basic_tests.jl
@@ -13,8 +13,8 @@ include(srcdir("hysteresisFunctions.jl"))
         spaceDims = (i,i)
         test_model = initialize(;seed=1234,space=Agents.GridSpace(spaceDims;periodic=true,metric = :euclidean))
         tagent=rand(test_model.agents)[2]
-        @test typeof(test_model) == AgentBasedModel{GridSpace{2, true, Nothing}, CarOwner, typeof(Agents.Schedulers.fastest), ModelParameters, MersenneTwister}
-        @test typeof(tagent) == CarOwner
+        @test typeof(test_model) == AgentBasedModel{GridSpace{2, true, Nothing}, DecisionAgent, typeof(Agents.Schedulers.fastest), ModelParameters, MersenneTwister}
+        @test typeof(tagent) == DecisionAgent
     end
 end
 
@@ -23,50 +23,10 @@ test_model = initialize(;seed=1234,space=Agents.GridSpace(spaceDims;periodic=tru
 @testset "agentFunctions.jl" begin
     for i_agent in rand(test_model.agents,100)
         tagent = i_agent[2]
-        @testset "getters and setters" begin
-            @test get_car_price(0,test_model) ==test_model.priceCombustionCar
-            @test get_car_price(1,test_model) ==test_model.priceElectricCar
             test_iterations  = 100
             for state in [0,1]
                 set_state!(state,tagent)
                 @test tagent.state == state
             end
-            for carValue in rand(0:0.5:10000,test_iterations)
-                set_carValue!(carValue,tagent)
-                @test tagent.carValue == carValue
-                set_purchaseValue!(carValue,tagent)
-                @test tagent.purchaseValue == carValue
-            end
-            for carAge in rand(0:100,test_iterations)
-                set_carAge!(carAge,tagent)
-                @test tagent.carAge == carAge
-            end
-            for budgetChange in rand(0:10000,test_iterations)
-                previousBudget = tagent.budget
-                update_budget!(budgetChange,tagent)
-                @test tagent.budget == previousBudget-budgetChange
-            end
-            for new_budget in rand(0:0.25:10000,test_iterations)
-                set_budget!(new_budget,tagent)
-                @test tagent.budget == new_budget
-            end
-        end
-    end
-end
-
-@testset "cost functions" begin
-    random_agents = rand(test_model.agents,1000)
-    for i_agent in random_agents
-        i_agent = i_agent[2]
-        fuelCostKM = (i_agent.state ===0 ? test_model.fuelCostKM : test_model.powerCostKM)
-        maintenanceCostKM = (i_agent.state ===0 ? test_model.maintenanceCostCombustionKM : test_model.maintenanceCostElectricKM)
-        yearly_cost = yearly_car_cost(i_agent.kilometersPerYear,i_agent.carAge,fuelCostKM,maintenanceCostKM)
-        @test yearly_cost >= 0
-        usageYears=25
-        multi_year_cost = multi_year_car_cost(i_agent.kilometersPerYear,usageYears,i_agent.carAge,i_agent.state,test_model)
-        @test multi_year_cost >= yearly_cost*(usageYears-i_agent.carAge)
-        average_cost = average_car_cost(i_agent.kilometersPerYear, usageYears,i_agent.carAge, i_agent.state, test_model)
-        @test multi_year_cost/(usageYears-i_agent.carAge) == average_cost
-        @test depreciate_car_value(i_agent,test_model) <= i_agent.carValue
     end
 end
