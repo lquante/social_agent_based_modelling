6718e2ba6939299e33a9e8f8f6742f40d4a2e78d
lquante <lennart.quante@me.com>
2021-10-19 12:41:22 +0200

    inital enabling of distributed ensemble simulation

--- scripts/SIR_ensemble_simulations.jl
+++ scripts/SIR_ensemble_simulations.jl
@@ -1,55 +1,59 @@
 using DrWatson
 @quickactivate "Social Agent Based Modelling"
-using Agents, Random, DataFrames, LightGraphs
-using Distributions: Poisson, DiscreteNonParametric
-using GraphPlot
-using DelimitedFiles
+using Distributed
+numberCPUS = length(Sys.cpu_info()) # get number of available CPUS
+addprocs(numberCPUS-1; exeflags="--project") # avoiding non-initialized project on the distributed workers (https://stackoverflow.com/questions/60934852/why-does-multiprocessing-julia-break-my-module-imports)
 
-include(srcdir("agentFunctions.jl"))
-include(srcdir("modelling.jl"))
-include(srcdir("hysteresisFunctions.jl"))
+@everywhere begin
+    using DrWatson
+    using Agents, Random, DataFrames, LightGraphs
+    using Distributions: Poisson, DiscreteNonParametric
+    using GraphPlot
+    using DelimitedFiles
 
-Random.seed!(1234)
+    include(srcdir("agentFunctions.jl"))
+    include(srcdir("modelling.jl"))
+    include(srcdir("hysteresisFunctions.jl"))
 
-# define model(s) to be used
-network_number = 1000
-network_seeds = rand((0:10000),network_number)
+    Random.seed!(1234)
 
-ensemble_spaces = [] #TODO: define properly typed container
-#network params
-k = 5 # number of neighbours of each node before randomization if even, otherwise k-1
-beta = 0.1 # probability for an edge to be not rewired to another node, i.e. 1-beta edges will be rewired
-node_number = 1000 # number of nodes in the random networks
+    # define model(s) to be used
+    network_number = 1000
+    network_seeds = rand((0:10000),network_number)
 
-for i_seed in network_seeds
-    push!(ensemble_spaces,Agents.GraphSpace(watts_strogatz(node_number,k,beta,seed=i_seed)))
-end
+    ensemble_spaces = [] #TODO: define properly typed container
+    #network params
+    k = 5 # number of neighbours of each node before randomization if even, otherwise k-1
+    beta = 0.1 # probability for an edge to be not rewired to another node, i.e. 1-beta edges will be rewired
+    node_number = 1000 # number of nodes in the random networks
+
+    for i_seed in network_seeds
+        push!(ensemble_spaces,Agents.GraphSpace(watts_strogatz(node_number,k,beta,seed=i_seed)))
+    end
 
 # set parameters to be varied in the ensemble
-parameters = Dict(
-	:space => ensemble_spaces,
-	:switchingLimit => node_number*0.01, # assuming that 1 percent of population can be vaccinated per timestep
-    :schedulerIndex => [1], #only standard fastest scheduler by agent id, no affinity ordering (index 2) or lowAffinityFirst (index 3)
-	:neighbourhoodExtent => 1,
-	:switchingBoundary => [0.5,0.7,0.9], #varying vaccine decision boundary to check for sensitivity
-	:seed => 1910, # fixed seed to to enough variation by network composition
-    #SIR parameters
-    :detectionTime => 7,
-	:initialInfected => 0.1,
-	:deathRate => 0.04,
-	:reinfectionProtection => 180,
-	:transmissionUndetected => 0.2,
-	:transmissionDetected => 0.02
-)
+    parameters = Dict(
+        :space => ensemble_spaces,
+        :switchingLimit => node_number*0.01, # assuming that 1 percent of population can be vaccinated per timestep
+        :schedulerIndex => [1], #only standard fastest scheduler by agent id, no affinity ordering (index 2) or lowAffinityFirst (index 3)
+        :neighbourhoodExtent => 1,
+        :switchingBoundary => [0.5,0.7,0.9], #varying vaccine decision boundary to check for sensitivity
+        :seed => 1910, # fixed seed to to enough variation by network composition
+        #SIR parameters
+        :detectionTime => [3,7,10],
+        :initialInfected => [0.025,0.05,0.1],
+        :deathRate => 0.04,
+        :reinfectionProtection => 180,
+        :transmissionUndetected => 0.2,
+        :transmissionDetected => 0.02
+    )
 
-# data to be tracked for each agent
+    # data to be tracked for each agent
 
-adata = [:state,:affinity,:SIR_status]
-
+    adata = [:state,:affinity,:SIR_status]
+end
 
 # perform parameter scan for varying models
-ensemble_agent_data_frame, ensemble_model_data = paramscan(parameters, initialize_SIR; adata,agent_step! = agent_step_SIR!,model_step!, n = 100)
-
+ensemble_agent_data_frame, ensemble_model_data = paramscan(parameters, initialize_SIR; adata,agent_step! = agent_step_SIR!,model_step!, n = 100, parallel = true)
 # safe to datadir
-ensembleidentifier="test01"
-CSV.write(datadir("ensemble_"+ensembleidentifier+".csv"),ensemble_agent_data_frame)
+CSV.write(datadir("ensemble_test.csv"),ensemble_agent_data_frame)
