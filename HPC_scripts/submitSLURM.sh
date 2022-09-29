823888e64f8be202a66f1cd0ba4c907bf61bedf8
lquante <lennart.quante@me.com>
2022-09-29 11:49:59 +0200

    correcting paths for simulations

--- HPC_scripts/submitSLURM.sh
+++ HPC_scripts/submitSLURM.sh
@@ -6,9 +6,9 @@
 #SBATCH --account=compacts
 #SBATCH --output=log/%j.out
 #SBATCH --error=log/%j.err
-#SBATCH --workdir=/p/projects/compacts/projects/DeMo/Damians_code/social_agent_based_modelling/
+#SBATCH --workdir=/p/projects/compacts/projects/DeMo/social_agent_based_modelling/
 #SBATCH --time=0-04:00:00
 
 module load julia/1.6.1
-simulation='/p/projects/compacts/projects/DeMo/Damians_code/social_agent_based_modelling/scripts/social_ensembles.jl'
+simulation='/p/projects/compacts/projects/DeMo/social_agent_based_modelling/scripts/social_ensembles.jl'
 srun julia $simulation 100 #change ensemble size via argument
