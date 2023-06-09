3af09f77743f45ba410b12710763c212bedbb13d
lquante <lennart.quante@me.com>
2023-06-09 11:26:58 +0200

    forgot end

--- scripts/self_reliance_ensembles.jl
+++ scripts/self_reliance_ensembles.jl
@@ -53,4 +53,5 @@ for mu_p in mu_range:
     for sigma_p in sigma_range:  
         @printf "Simulation running with normal distributed self-reliance mu=%.2f, sigma=%.2f\n" mu_p sigma_p
         Simulate(;mu=mu, sigma=sigma)
+    end
 end
