68c9d6dbc311788a5ab88a468bcd48a6ad675384
lquante <lennart.quante@me.com>
2023-07-27 17:35:27 +0200

    expanding simulation time

--- HPC_scripts/submitSLURM.sh
+++ HPC_scripts/submitSLURM.sh
@@ -7,7 +7,7 @@
 #SBATCH --output=log/%j.out
 #SBATCH --error=log/%j.err
 #SBATCH --workdir=/p/projects/compacts/projects/DeMo/social_agent_based_modelling/
-#SBATCH --time=0-05:00:00
+#SBATCH --time=0-23:59:00
 
 module load julia/1.6.1
 simulation='/p/projects/compacts/projects/DeMo/social_agent_based_modelling/scripts/self_reliance_ensembles.jl'
